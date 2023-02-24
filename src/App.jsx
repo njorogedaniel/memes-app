5b88a7c781f0bfed224662e5495c808d1c9e5fa7
Daniel <dnjoroge96.dn@gmail.com>
2023-02-24 09:27:12 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+// import './App.css'
+import './style.css'
+import Header from './components/Header'
+import Content from './components/Content'
+
+function App() {
+  
+
+  return (
+    <div className='container'>
+      <Header />
+      <Content />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Header() {
+    return(
+        <div className="header">
+            <div className="logo">
+                <img id="troll-face"src="./troll-face.png" alt="troll-face"></img>
+                <h3 id="brand">Meme Generator</h3>
+            </div>
+            <p id="sub">React 3</p>
+        </div>
+        
+
+
+    )
+}
+
+
